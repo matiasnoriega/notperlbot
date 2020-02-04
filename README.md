2837554fee641199705c09905ab77842d71c3a43
Eduardo Casarero <eduardo.casarero@percona.com>
2020-02-03 23:28:27 -0300

    some stuff

---
+++ README.md
@@ -0,0 +1,6 @@
+pip install < requirements.txt
+cd errbot
+# to get some initial configurations for local develompent
+cd errbot
+errbot --init
+errbot -T
---
+++ errbot/plugins/err-example/test_example.py
@@ -0,0 +1,7 @@
+pytest_plugins = ["errbot.backends.test"]
+
+extra_plugin_dir = '.'
+
+def test_tryme(testbot):
+    testbot.push_message('!tryme')
+    assert 'It works !' in testbot.pop_message()
