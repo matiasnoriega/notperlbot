a079297a6a8457d9cda10cceabc3c7e2e09cd5f9
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2020-02-06 18:48:05 -0300

    fix syntax errors through pep8

--- errbot/plugins/roll/roll.py
+++ errbot/plugins/roll/roll.py
@@ -1,23 +1,24 @@
 from errbot import BotPlugin, botcmd
 import random
 
-class Roll(BotPlugin):
-  
-    @botcmd
-    def roll(self,msg,args):
-        
-        numbers = args.split('d',1)
 
-        if not numbers[0].isdigit() == True:
+class Roll(BotPlugin):
+
+    @botcmd
+    def roll(self, msg, args):
+
+        numbers = args.split('d', 1)
+
+        if not numbers[0].isdigit() is True:
             return "la cantidad de dados debe ser un numero"
-        if not numbers[1].isdigit() == True or numbers[1] == '0':
+        if not numbers[1].isdigit() is True or numbers[1] is '0':
             return "la cantidad de caras debe ser un numero distinto de 0"
-        
+
         dados = int(numbers[0])
         caras = int(numbers[1])
         tirada = ""
 
         for i in range(dados):
             tirada += str(random.randint(1, caras)) + ' '
-        
+
         return tirada
