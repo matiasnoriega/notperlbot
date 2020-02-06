06e140311f25e5499578e0b2531c8b0548833b75
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2020-02-06 14:21:58 -0300

    integer and zero check, complete roll return

--- errbot/plugins/err-example/roll.py
+++ errbot/plugins/err-example/roll.py
@@ -1,5 +1,5 @@
 from errbot import BotPlugin, botcmd
-
+import random
 
 class Roll(BotPlugin):
   
@@ -10,7 +10,14 @@ class Roll(BotPlugin):
 
         if not numbers[0].isdigit() == True:
             return "la cantidad de dados debe ser un numero"
-        if not numbers[1].isdigit() == True:
-            return "la cantidad de caras debe ser un numero"
+        if not numbers[1].isdigit() == True or numbers[1] == '0':
+            return "la cantidad de caras debe ser un numero distinto de 0"
+        
         dados = int(numbers[0])
         caras = int(numbers[1])
+        tirada = ""
+
+        for i in range(dados):
+            tirada += str(random.randint(1, caras)) + ' '
+        
+        return tirada
