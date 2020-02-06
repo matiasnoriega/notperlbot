23b107afd7a894972de588454101938c15fae7df
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2020-02-06 18:49:44 -0300

    add test case

--- errbot/plugins/roll/test_roll.py
+++ errbot/plugins/roll/test_roll.py
@@ -1,9 +1,10 @@
+import re
 pytest_plugins = ["errbot.backends.test"]
 
 extra_plugin_dir = '.'
 
+
 def test_roll(testbot):
     testbot.push_message('!roll 2d20')
-    assert '^(\\d{1,2}\\s)+$' in testbot.pop_message()
-    #testbot.assertInCommand('!roll 1d20','^(\\d{2}\\s)+$')
-    #assert re.match('^(\\d{1,2}\\s)+$', testbot.pop_message())
+    test_value = re.compile('^(\\d{1,2}\\s*)+$')
+    assert test_value.match(testbot.pop_message())
