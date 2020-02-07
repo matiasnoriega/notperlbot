a27d6bf67e7c40590f1c6fdb511dfdfe8480ecbe
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2020-02-07 13:48:04 -0300

    add string validation

--- errbot/plugins/roll/roll.py
+++ errbot/plugins/roll/roll.py
@@ -1,5 +1,6 @@
 from errbot import BotPlugin, botcmd
 import random
+import re
 
 
 class Roll(BotPlugin):
@@ -7,20 +8,26 @@ class Roll(BotPlugin):
     @botcmd
     def roll(self, msg, args):
 
-        numbers = args.split('d', 1)
+        d = re.match('^[0-9]+d[0-9]+$', args)
+       
+        if d:
 
-        if len(numbers) != 2:
-            return "poneme la cantidad de caras del dado, no soy mago"
-        if not numbers[0].isdigit() == True:
-            return "la cantidad de dados debe ser un numero"
-        if not numbers[1].isdigit() == True or numbers[1] == '0':
-            return "la cantidad de caras debe ser un numero distinto de 0"
+            numbers = args.split('d', 1)
 
-        dados = int(numbers[0])
-        caras = int(numbers[1])
-        tirada = ""
+            if len(numbers) != 2:
+                return "poneme la cantidad de caras del dado, no soy mago"
+            if not numbers[0].isdigit() == True:
+                return "la cantidad de dados debe ser un numero"
+            if not numbers[1].isdigit() == True or numbers[1] == '0':
+                return "la cantidad de caras debe ser un numero distinto de 0"
 
-        for i in range(dados):
-            tirada += str(random.randint(1, caras)) + ' '
+            dados = int(numbers[0])
+            caras = int(numbers[1])
+            tirada = ""
 
-        return tirada
+            for i in range(dados):
+                tirada += str(random.randint(1, caras)) + ' '
+
+            return tirada
+        else:
+            return "el formato es '!roll XdY', donde X es la cantidad de dados e Y la cantidad de caras"
