0c9382db4b5ccd1c762f5ae898a7174a62dfafce
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2020-02-07 14:07:13 -0300

    remove unnecessary validations

--- errbot/plugins/roll/roll.py
+++ errbot/plugins/roll/roll.py
@@ -9,16 +9,12 @@ class Roll(BotPlugin):
     def roll(self, msg, args):
 
         d = re.match('^[0-9]+d[0-9]+$', args)
-       
+
         if d:
 
             numbers = args.split('d', 1)
 
-            if len(numbers) != 2:
-                return "poneme la cantidad de caras del dado, no soy mago"
-            if not numbers[0].isdigit() == True:
-                return "la cantidad de dados debe ser un numero"
-            if not numbers[1].isdigit() == True or numbers[1] == '0':
+            if numbers[1] == '0':
                 return "la cantidad de caras debe ser un numero distinto de 0"
 
             dados = int(numbers[0])
